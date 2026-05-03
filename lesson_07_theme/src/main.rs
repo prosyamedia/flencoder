971bc7331eed9f8990a20ad69f7de334857d55d0
Anmol Mishra <anmol@prosya.com>
2026-05-03 13:04:29 +1000

    Lesson 7 - add callback and correct resize

--- lesson_07_theme/src/main.rs
+++ lesson_07_theme/src/main.rs
@@ -16,7 +16,29 @@ fn main() {
     let mut ui = UserInterface::init();
 
     // Set main window resizable to invisible group that covers all other groups
-    ui.main_window.resizable(&ui.resize_group);
+    //ui.main_window.resizable(&ui.resize_group);
+    let min_w = ui.main_window.w();
+    let min_h = ui.main_window.h();
+
+    ui.main_window.resize_callback(move |win, x, y, w_new, h_new| {
+        let mut w = w_new;
+        let mut h = h_new;
+
+        if w_new < min_w {
+            w = min_w;
+        }
+        if h_new < min_h {
+            h = min_h;
+        }
+
+        // Apply corrected size
+        // Only resize if we are correcting the size
+        if w != w_new || h != h_new {
+            win.resize(x, y, w, h);
+        }
+    });
+
+
     ui.main_window.show();
     let _=app.run();
 }
